b91839d58afb8239489b5a79899a944f7ccbabc4
Pieter Wolfert <pieter@pieterwolfert.com>
2020-12-06 11:46:38 +0100

    shorter

--- 06/day06.py
+++ 06/day06.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
 
-customs = [len(set(filter(None, i.replace('\n', '')))) for i in open('input.txt').read().split('\n\n')]
-print(sum(customs))
+print(sum([len(set(filter(None, i.replace('\n', '')))) for i in open('input.txt').read().split('\n\n')]))
