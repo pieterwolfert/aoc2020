e32b726f6e2a1cb8b4bb3ef66e7d3941a49f5ecc
Pieter Wolfert <pieter@pieterwolfert.com>
2020-12-08 16:42:15 +0100

    netter

--- 08/day08_2.py
+++ 08/day08_2.py
@@ -1,29 +1,32 @@
 #!/env/user/bin python3
 
-
+import time
 
 def main():
     with open('input.txt') as f:
         instructions = [i.strip().split(' ') for i in f.readlines()]
     nop_jump = [ind for ind, x in enumerate(instructions) if 'nop' in x or 'jmp' in x]
     for item in nop_jump:
-        lcop = [i.copy() for i in instructions]
-        if 'jmp' in lcop[item][0]:
-            lcop[item][0] = 'nop'
+        loop = True
+        if 'jmp' in instructions[item][0]:
+            instructions[item][0] = 'nop'
+            acc, loop = checkprogram(instructions)
+            instructions[item][0] = 'jmp'
         else:
-            lcop[item][0] = 'jmp'
-        acc, loop = checkprogram(lcop)
+            instructions[item][0] = 'jmp'
+            acc, loop = checkprogram(instructions)
+            instructions[item][0] = 'nop'
         if not loop:
             print(acc)
 
 def checkprogram(instructions):
-    indices = []
+    indices = set()
     step = 0
     acc = 0
     while step not in indices:
+        indices.add(step)
         if step >= len(instructions):
             return acc, False
-        indices.append(step)
         if 'acc' in instructions[step]:
             acc += int(instructions[step][1])
             step += 1
@@ -36,6 +39,9 @@ def checkprogram(instructions):
     return acc, False
 
 if __name__=="__main__":
+    t0 = time.time()
     main()
+    t1 = time.time()
+    print(t1-t0)
 
 
